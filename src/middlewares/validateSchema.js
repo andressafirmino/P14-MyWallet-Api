5d8afbb470327d87d8f8cc42b6f4a35ffb0de125
andressafirmino <andressafirmino78@gmail.com>
2023-07-10 20:37:17 -0300

    Versão POST/new-transaction 3

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,8 +1,8 @@
 export function validateSchema(schema) {
     return (req, res, next) => {
-        const validateSignUp = schema.validate(req.body, { abortEarly: false });
-        if (validateSignUp.error) {
-            const errors = validateSignUp.error.details.map(detail => detail.message);
+        const validate = schema.validate(req.body, { abortEarly: false });
+        if (validate.error) {
+            const errors = validate.error.details.map(detail => detail.message);
             console.log(errors)
             return res.status(422).send(errors);
         }
@@ -12,9 +12,9 @@ export function validateSchema(schema) {
 
 export function validateSchemaHeader(schema) {
     return (req, res, next) => {
-        const validateSignUp = schema.validate(req.params, { abortEarly: false });
-        if (validateSignUp.error) {
-            const errors = validateSignUp.error.details.map(detail => detail.message);
+        const validate = schema.validate(req.params, { abortEarly: false });
+        if (validate.error) {
+            const errors = validate.error.details.map(detail => detail.message);
             
             return res.status(422).send(errors);
         }
